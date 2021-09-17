3f6a25498b43ab0cede28f56d41e6fb38de4c0a4
adniasalsabila <adniasalsabila8@gmail.com>
2021-09-17 08:57:16 +0700

    Tugas12_Commit

---
+++ app/src/main/java/com/gits/authapp/ListAdapter.kt
@@ -0,0 +1,36 @@
+package com.gits.authapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter(private val context: Context, private val list: ArrayList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        internal var tvJudul: TextView
+
+        init {
+            tvJudul = itemView.findViewById(R.id.textViewJudul)
+        }
+
+        internal fun bind(position: Int) {
+            tvJudul.text = list[position]
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_list, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ViewHolder).bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
